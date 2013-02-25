c2e731f8044250145b487480f0d34e80f5136a63
emil-sourcebits <emil.joswin@sourcebits.com>
2013-02-25 11:12:51 +0530

    1a success

--- 1a.py
+++ 1a.py
@@ -4,13 +4,17 @@ m_rate = float(raw_input('min monthly : '))
 
 
 def calculate(balance, a_rate, m_rate):
+    a = round(a_rate/12)
+    total = 0
     for i in range(12):
-        monthly = balance*m_rate
-        print 'month : ', i + 1
-        print 'monthly ', round(monthly,2)
-        print 'remaining balance', balance - monthly
-        balance -= (monthly - balance*a_rate/12)
-
+        monthly = round(balance*m_rate,2)
+        balance -= (monthly - round((balance-monthly)*a_rate/12, 2))
+	total += monthly
+        print 'Month : ', i + 1
+        print 'Minimum monthly payment ', round(monthly,2)
+        print 'Remaining balance', balance 
+    print 'Total paid:', total
+    print 'Remaining Balance:', balance
 
 
 
